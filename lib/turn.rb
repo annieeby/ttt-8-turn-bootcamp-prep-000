54650b570559cdaf3ce29d71276e4dd18f92f219
Annie Eby <eby.annie@gmail.com>
2018-09-18 19:14:33 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -30,7 +30,7 @@ def move(board, index, character = "X")
   board[index] = character
 end
 
-def turn(board, index)
+def turn(board)
   input = gets.strip
   input_to_index(input)
   if valid_move?(board, index)
