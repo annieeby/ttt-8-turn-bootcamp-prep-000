7abcb9225eaa61a5bd49d89468e107927f615edd
Annie Eby <eby.annie@gmail.com>
2018-09-18 18:07:00 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,2 +1,10 @@
-def display_board
+def display_board(board)
   puts " #{0} | #{1} | #{2} | "
+  puts "-----------"
+  puts " #{3} | #{4} | #{5} | "
+  puts "-----------"
+  puts " #{6} | #{7} | #{8} | "
+end
+
+def valid_move?
+  
