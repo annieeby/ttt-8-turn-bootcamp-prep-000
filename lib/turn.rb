c42a114ba169fe251c348a009192aed2496616ac
Annie Eby <eby.annie@gmail.com>
2018-09-18 19:03:49 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -30,8 +30,8 @@ def move(board, index, character = "X")
   board[index] = character
 end
 
-def turn(board)
-  while !valid_move?(board)
+def turn(board, index)
+  while !valid_move?(board, index)
   puts "Please enter 1-9:"
 end
 end
